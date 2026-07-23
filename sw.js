58944ed29aaca368efeeefd2dde8af82f25cac8f
Рамазан Асылбек <A.Kairat@corp.alkz.kz>
2026-07-23 08:44:01 +0500

    Refresh client cache for participant picker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "ppr-v209-tested-fixes";
+const CACHE_NAME = "ppr-v210-collaborative-participants";
 const ASSETS = [
   "./",
   "./index.html",
@@ -9,7 +9,7 @@ const ASSETS = [
   "./modules/requests.js?v=288-print-request-pages",
   "./modules/comments.js?v=288-print-request-pages",
   "./modules/director.js?v=288-print-request-pages",
-  "./app.js?v=209-tested-fixes",
+  "./app.js?v=210-collaborative-participants",
   "./node_modules/jsqr/dist/jsQR.js?v=313-spelling-fixes",
   "./manifest.json",
   "./icon.svg",
