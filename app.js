abecccae3603be61ed9307b7b72532e252ea3495
Рамазан Асылбек <A.Kairat@corp.alkz.kz>
2026-09-07 17:50:26 +0500

    Simplify repeat grouping on mobile

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ const PROFILE_KEY = "ppr-pwa-profile-v1";
 const USERS_KEY = "ppr-pwa-users-v1";
 const EDITOR_PREVIEW_ROLE_KEY = "ppr-editor-preview-role-v1";
 const EDITOR_PREVIEW_AREA_KEY = "ppr-editor-preview-area-v1";
-const APP_VERSION = "v805-repeat-failure-print-clean";
+const APP_VERSION = "v806-repeat-failure-mobile-simple";
 document.querySelector("#loginVersion")?.replaceChildren(APP_VERSION);
 
 const ensurePprOptionalLibrary = window.PprPrintAssets.createOptionalLibraryLoader(APP_VERSION);
